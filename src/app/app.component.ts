08593583308f7d27f55165b6f5d8a300a983f761
Victoria <victoriabarbero88@gmail.com>
2020-12-18 12:54:37 +0100

    commit before deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { OnInit, Component, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatSort, Sort} from '@angular/material/sort';
+import animalsData from './animals.json';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'filterlist';
+export class AppComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'name', 'sex', 'genus', 'age', 'birth'];
+  dataSource = new MatTableDataSource(animalsData);
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  ngOnInit() {
+    this.dataSource.sort = this.sort;
+    const sortState: Sort = {active: 'sex', direction: 'asc'};
+    this.sort.active = sortState.active;
+    this.sort.direction = sortState.direction;
+    this.sort.sortChange.emit(sortState);
+  }
 }
